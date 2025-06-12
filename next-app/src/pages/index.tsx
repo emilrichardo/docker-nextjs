3e42c20e6265127a25fd23ba71074d3582412151
{emilrichardo} <{emilrichardo@gmail.com}>
2025-06-12 13:57:51 +0000

    remove multistage

--- next-app/src/pages/index.tsx
+++ next-app/src/pages/index.tsx
@@ -6,19 +6,16 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next app</title>
+        <title>Keys Neuraz</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js</a> on Docker Compose!
+          Welcome to Keys Neuraz
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
+
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
